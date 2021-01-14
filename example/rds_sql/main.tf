c1d0fba0e088b34e47bde3b8cb53fbfef948ed74
Nyein Chan Aung <ninja.aung02@gmail.com>
2021-01-13 17:40:45 -0800

    Finished db instance file

--- example/rds_sql/main.tf
+++ example/rds_sql/main.tf
@@ -1,14 +1,22 @@
 resource "aws_db_instance" "db" {
-    allocated_storage    = 20
-    storage_type         = "gp2"    # General Purpose SSD
-    engine               = "mysql" 
-    engine_version       = "8.0"    # mysql v8.0
-    instance_class       = "db.t2.micro"
-    name                 = "Terraform SQL DataBase"
-    username             = "Terraom"
-    password             = "password"
-    tag                  = {
-        Name = "SQL Database" 
-    }
+    identifier                  = "terraform-database"    # DB Identifier on console
+    allocated_storage           = 20
+    storage_type                = "gp2"             # General Purpose SSD
+    engine                      = "mysql" 
+    engine_version              = "8.0"             # mysql v8.0
+    instance_class              = "db.t2.micro"     # Free tier t2.micro
+    name                        = "test_db"
+    username                    = "Terraform"        
+    password                    = "password"
+    multi_az                    = true
+    skip_final_snapshot         = true  # if ommitted Finalsnahotid throws an error
+    backup_retention_period     = 0
+    backup_window               = "06:00-09:00"
+    delete_automated_backups    = true  # deletes backups with DB
+    deletion_protection         = false
+
     
 }
+
+# 1. automated daily backup of DB
+# 1. available to use across multiple availability zones
