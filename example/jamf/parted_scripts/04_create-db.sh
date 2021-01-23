32236edae3cab3bd95df1bcee3f948c4e0ae3c4a
Nyein Chan Aung <ninja.aung02@gmail.com>
2021-01-22 19:01:56 -0800

    fixed input

--- example/jamf/parted_scripts/04_create-db.sh
+++ example/jamf/parted_scripts/04_create-db.sh
@@ -10,33 +10,33 @@ source .env
 ### SCRIPT LOGIC ###
 ####################
 
-# Check Jamf Pro Server Tools CLI version
-jamf-pro --version
+# # Check Jamf Pro Server Tools CLI version
+# jamf-pro --version
 
-# Create Jamf Pro Database and MySQL User Account
-sudo jamf-pro database init
+# # Create Jamf Pro Database and MySQL User Account
+# sudo jamf-pro database init
 
-# Enter your MySQL root password
-read -sp "Your MySQL root password:  " password
+# # Enter your MySQL root password
+# read -sp "Your MySQL root password:  " password
 
-# Set new MySQL root password, press Enter to skip
-read -sp "MySQL root password, leave blank to skip" new_password
+# # Set new MySQL root password, press Enter to skip
+# read -sp "MySQL root password, leave blank to skip" new_password
 
 
-read -p "Jamf DB Name: " db_name
-read -p "Jamf Username: " username
+# read -p "Jamf DB Name: " db_name
+# read -p "Jamf Username: " username
 
 # Press "Y" if you want to save the database connection settings to the Jamf Pro configuration (DataBase.xml)
-read -p "Save to Jamf Pro Config (DataBase.xml): y/n" yn
-if [yn == "y"]
+read -p "Save to Jamf Pro Config (DataBase.xml): y/n " yn
+if [ $yn == "y" ]
 then
     echo "Saved Connection"
 fi
 
 
 # Pres "Y" if you want to save the username and password to the Jamf Pro Server Tools CLI configuration file
-read -p "Save username and password to Jamf Sever CLI config File: y/n" yn
-if [yn == "y"]
+read -p "Save username and password to Jamf Sever CLI config File: y/n " yn
+if [ $yn == "y" ]
 then
     echo "Save Creds to Jamf Server"
 fi
