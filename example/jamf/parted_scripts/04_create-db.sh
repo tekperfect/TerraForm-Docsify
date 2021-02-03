8477ab258aac35d942fdb42cba90f0a4df7c15ad
NinjaAung <ninja.aung02@gmail.com>
2021-02-02 17:16:24 -0800

    Added Jamf Database setup

--- example/jamf/parted_scripts/04_create-db.sh
+++ example/jamf/parted_scripts/04_create-db.sh
@@ -10,25 +10,24 @@ source .env
 ### SCRIPT LOGIC ###
 ####################
 
-# # Enter your MySQL root password
-read -sp "Your MySQL root password:  " password
+
+# # Press "Y" if you want to save the database connection settings to the Jamf Pro configuration (DataBase.xml)
+# read -p "Save to Jamf Pro Config (DataBase.xml): y/n " yn
+# if [ $yn == "y" ]
+# then
+#     echo "Saved Connection"
+# fi
 
 
-read -p "Jamf DB Name: " db_name
-read -p "Jamf Username: " username
+# # Pres "Y" if you want to save the username and password to the Jamf Pro Server Tools CLI configuration file
+# read -p "Save username and password to Jamf Sever CLI config File: y/n " yn
+# if [ $yn == "y" ]
+# then
+#     echo "Save Creds to Jamf Server"
+# fi
 
-# Press "Y" if you want to save the database connection settings to the Jamf Pro configuration (DataBase.xml)
-read -p "Save to Jamf Pro Config (DataBase.xml): y/n " yn
-if [ $yn == "y" ]
-then
-    echo "Saved Connection"
-fi
-
-
-# Pres "Y" if you want to save the username and password to the Jamf Pro Server Tools CLI configuration file
-read -p "Save username and password to Jamf Sever CLI config File: y/n " yn
-if [ $yn == "y" ]
-then
-    echo "Save Creds to Jamf Server"
-fi
-
+# Jamf Database Init?
+mysql -u "root" -e "CREATE DATABASE jamfsoftware;"
+mysql -u "root" -e "CREATE USER '${database_user}'@'localhost' IDENTIFIED WITH mysql_native_password BY '${password}';"
+mysql -u "root" -e "GRANT ALL ON jamfsoftware.* TO '${database_user}'@'localhost';"
+mysql -u "root" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${mysql_root_password}';"
